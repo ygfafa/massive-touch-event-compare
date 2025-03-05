e2499d5cdff402d5d2a4f3f05f5de46fc0f6c72b
박종혁 <jonghyeok@jobis.co>
2025-03-05 22:34:24 +0900

    fix: build error

--- app/szs/_stores/gameResultStore.ts
+++ app/szs/_stores/gameResultStore.ts
@@ -1,19 +1,13 @@
-import { create } from 'zustand'
-
-import { PrizeType } from '~/app/(rewards)/rewards/taptap/_constants'
+import { create } from "zustand";
 
 type GameResultState = {
-  touchCount?: number
-  prizeType?: PrizeType
-  setTouchCount: (count: number) => void
-  setPrizeType: (type: PrizeType) => void
-  reset: () => void
-}
+  touchCount?: number;
+  setTouchCount: (count: number) => void;
+  reset: () => void;
+};
 
 export const useGameResultStore = create<GameResultState>((set) => ({
   touchCount: undefined,
-  prizeType: undefined,
   setTouchCount: (count) => set({ touchCount: count }),
-  setPrizeType: (type) => set({ prizeType: type }),
-  reset: () => set({ touchCount: undefined, prizeType: undefined }),
-}))
+  reset: () => set({ touchCount: undefined }),
+}));
