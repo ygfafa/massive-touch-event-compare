5d48248fde382858a7826bcec09bca655498d349
박종혁 <jonghyeok@jobis.co>
2025-05-22 08:29:34 +0900

    클릭 테스트

---
+++ app/ios/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useState } from "react";
+
+const Page = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p
+        style={{
+          width: "100%",
+          textAlign: "center",
+          fontSize: 32,
+          fontWeight: 800,
+          marginBottom: 32,
+        }}
+      >
+        {count}
+      </p>
+
+      <div style={{ display: "flex", gap: 24 }}>
+        <button
+          style={{ padding: 42, backgroundColor: "lightgreen" }}
+          onClick={() => setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+          style={{ padding: 42, backgroundColor: "lightsalmon" }}
+          onClick={() => setCount(count + 1)}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
