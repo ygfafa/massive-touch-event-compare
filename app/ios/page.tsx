01c98a37603a660158abd10841f02fc169771460
박종혁 <jonghyeok@jobis.co>
2025-05-22 16:39:01 +0900

    클릭으로 수정

--- app/ios/page.tsx
+++ app/ios/page.tsx
@@ -20,14 +20,14 @@ const Page = () => {
       <div style={{ display: "flex", gap: 24 }}>
         <motion.button
           style={{ padding: 42, backgroundColor: "lightgreen" }}
-          onPointerDown={() => setCount(count + 1)}
+          onClick={() => setCount(count + 1)}
           whileTap={{ scale: 0.9 }}
         >
           +
         </motion.button>
         <motion.button
           style={{ padding: 42, backgroundColor: "lightsalmon" }}
-          onPointerDown={() => setCount(count + 1)}
+          onClick={() => setCount(count + 1)}
           whileTap={{ scale: 0.9 }}
         >
           -
